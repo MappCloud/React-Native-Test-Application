6168c13e1621f023eb8d7a96be2a94b33b20efc4
Miroljub Stoilkovic <miroljub.stoilkovic@mapp.com>
2021-03-03 17:14:37 +0100

    Added support for rich notifications.

---
+++ ios/ExtensionForExample/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ExtensionForExample
+//
+//  Created by Miroljub Stoilkovic on 03/03/2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
